4d0e33602daf050680a369c1a9a4dc4f40bd5eef
coherentus <75427478+coherentus@users.noreply.github.com>
2021-04-01 20:44:46 +0300

    locale ru-ru

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -9,7 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    #empty_value_display = EMPTY_VALUE_DISPLAY
+    empty_value_display = EMPTY_VALUE_DISPLAY
 
 
 class GroupAdmin(admin.ModelAdmin):
